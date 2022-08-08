311b296365eb9ed8a09770eb3c40c1645d12d36f
Kimberly Hubbard <khubbard@outlook.com>
2022-08-08 14:32:55 -0400

    Added first basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 const App = () => {
   return (
-    <div>
-      Hello World!
+    <div className="categories-container">
+      <div class="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div class="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div class="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div class="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Women's</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div class="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Men's</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      
     </div>
   );
 }
