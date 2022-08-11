5b0053bb528cedb22833d9be2f2389cb481937de
Kimberly Hubbard <khubbard@outlook.com>
2022-08-11 13:04:41 -0400

    Added context

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,6 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
+
+import { UserContext } from '../../contexts/user.context';
+
 import { createUserDocumentFromAuth, signInAuthWithEmailAndPassword, signInWithGooglePopup} from "../../utils/firebase/firebase.utils";
 
 import './sign-in-form.styles.scss';
@@ -15,6 +19,8 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
 
+    const {setCurrentUser} = useContext(UserContext);
+
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     }
@@ -28,7 +34,7 @@ const SignInForm = () => {
         event.preventDefault();
         try {
             const {user} = await signInAuthWithEmailAndPassword(email, password);
-            console.log(user);
+            setCurrentUser(user);
             resetFormFields();
         } catch (error) {
             switch(error.code) {
