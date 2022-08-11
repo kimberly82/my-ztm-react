cb7a84442ca13abd8a38623463bd3b028e4bf7ae
Kimberly Hubbard <khubbard@outlook.com>
2022-08-11 15:51:44 -0400

    Stopping point for Thrusday

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useContext} from 'react';
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
 
@@ -6,6 +6,8 @@ import {
     createAuthUserWithEmailAndPassword,
     createUserDocumentFromAuth
 } from '../../utils/firebase/firebase.utils';
+
+import { UserContext } from '../../contexts/user.context';
 
 import './sign-up-form.styles.scss';
 
@@ -20,6 +22,8 @@ const SignUpForm = () => {
 
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
+
+    const {setCurrentUser, currentUser} = useContext(UserContext);
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
@@ -36,6 +40,7 @@ const SignUpForm = () => {
         // Have we authenticated user with email and password
         try {
             const {user} = await createAuthUserWithEmailAndPassword(email, password);
+            setCurrentUser(user);
             await createUserDocumentFromAuth(user, {displayName});
             resetFormFields();
         } catch(error) {
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -8,7 +8,6 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
     const {currentUser} = useContext(UserContext);
-    console.log(currentUser);
     return (
       <Fragment>
         <div className='navigation'>
@@ -19,9 +18,16 @@ const Navigation = () => {
                 <Link className='nav-link' to='/shop'>
                     SHOP
                 </Link>
-                <Link className='nav-link' to='/auth'>
-                    SIGN IN
-                </Link>
+                {
+                    currentUser ? (
+                        <span className='nav-link'>SIGN OUT</span>
+                    ) : (
+                        <Link className='nav-link' to='/auth'>
+                            SIGN IN
+                        </Link>
+                    )
+                }
+                
             </div>
         </div>
         <Outlet />
