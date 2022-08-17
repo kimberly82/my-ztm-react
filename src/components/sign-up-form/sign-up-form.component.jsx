1e84bf38a470c346fc0025e838bcad1b9ad406bf
Kimberly Hubbard <khubbard@outlook.com>
2022-08-17 13:34:25 -0400

    Using firebase authentication listner for consolidation purposes.

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,4 @@
-import {useState, useContext} from 'react';
+import {useState} from 'react';
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
 
@@ -6,8 +6,6 @@ import {
     createAuthUserWithEmailAndPassword,
     createUserDocumentFromAuth
 } from '../../utils/firebase/firebase.utils';
-
-import { UserContext } from '../../contexts/user.context';
 
 import './sign-up-form.styles.scss';
 
@@ -22,8 +20,6 @@ const SignUpForm = () => {
 
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
-
-    const {setCurrentUser} = useContext(UserContext);
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
@@ -40,7 +36,6 @@ const SignUpForm = () => {
         // Have we authenticated user with email and password
         try {
             const {user} = await createAuthUserWithEmailAndPassword(email, password);
-            setCurrentUser(user);
             await createUserDocumentFromAuth(user, {displayName});
             resetFormFields();
         } catch(error) {
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect} from 'react'
 
-import { onAuthStateChangedListener } from '../utils/firebase/firebase.utils';
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
 
 // as the actual value you want to access
 export const UserContext = createContext({
@@ -14,7 +14,10 @@ export const UserProvider = ({ children }) => {
 
     useEffect(() => {
         const unsubscribe = onAuthStateChangedListener((user) => {
-            console.log(user);
+            if (user) {
+                createUserDocumentFromAuth(user);
+            }
+            setCurrentUser(user);
         });
 
         return unsubscribe;
